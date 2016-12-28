4e76ffb6f8a798bc6ef15f9ecaf1a366bcd53c1c
Timothy Palpant <tim@palpant.us>
2016-12-28 02:40:52 +0000

    add commit script for testing claw

---
+++ testclaw/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/stianeikeland/go-rpio"
+)
+
+const (
+	pinOnOffNumber = 4
+	pinDirectionNumber = 17
+)
+
+func main() {
+	err := rpio.Open()
+	if err != nil {
+		fmt.Printf("Error opening gpio! %s\n", err)
+		return
+	}
+
+	pinOnOff := rpio.Pin(pinOnOffNumber)
+	pinOnOff.Output()
+	pinDirection := rpio.Pin(pinDirectionNumber)
+	pinDirection.Output()
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	go func() {
+		<-c
+		pinOnOff.High()
+		rpio.Close()
+		os.Exit(0)
+	}()
+
+	for {
+		pinOnOff.Low()
+		time.Sleep(400 * time.Millisecond)
+		pinOnOff.High()
+		pinDirection.Toggle()
+		time.Sleep(600 * time.Millisecond)
+	}
+}
