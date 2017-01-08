d36ed64418c4e043330d9ce62fd728fa0e391f22
Timothy Palpant <tim@palpant.us>
2017-01-08 01:37:02 +0000

    more motors for kbcontroller

--- controllers/kbcontroller/kbcontroller.go
+++ controllers/kbcontroller/kbcontroller.go
@@ -20,22 +20,46 @@ import (
 
 // parseKBState converts the given keyboard controller State into
 // the desired motor state for the Claw.
-func parseKBState(key tcell.Key) *clawserver.SetClawStateRequest {
-	m1 := motor.State_STOPPED
-	m2 := motor.State_STOPPED
-	switch key {
+func parseKBState(event *tcell.EventKey) *clawserver.SetClawStateRequest {
+	motorStates := make([]motor.State, 5)
+	for i := range motorStates {
+		motorStates[i] = motor.State_STOPPED
+	}
+	led := false
+
+	switch event.Key() {
 	case tcell.KeyUp:
-		m2 = motor.State_FORWARD
+		motorStates[1] = motor.State_FORWARD
 	case tcell.KeyDown:
-		m2 = motor.State_BACKWARD
+		motorStates[1] = motor.State_BACKWARD
 	case tcell.KeyRight:
-		m1 = motor.State_BACKWARD
+		motorStates[0] = motor.State_BACKWARD
 	case tcell.KeyLeft:
-		m1 = motor.State_FORWARD
+		motorStates[0] = motor.State_FORWARD
+	case tcell.KeyRune:
+		switch event.Rune() {
+		case 'a':
+			motorStates[2] = motor.State_FORWARD
+		case 'd':
+			motorStates[2] = motor.State_BACKWARD
+		case 'w':
+			motorStates[3] = motor.State_FORWARD
+		case 's':
+			motorStates[3] = motor.State_BACKWARD
+		case 'q':
+			motorStates[4] = motor.State_FORWARD
+		case 'e':
+			motorStates[4] = motor.State_BACKWARD
+		case 'f':
+			led = true
+		case 'r':
+			led = false
+		}
 	}
 
 	return &clawserver.SetClawStateRequest{
-		MotorStates: []motor.State{m1, m2},
+		MotorStates: motorStates,
+		Led: led,
 	}
 }
 
@@ -83,8 +107,8 @@ Loop:
 			case tcell.KeyEscape:
 				break Loop
 			default:
-				glog.V(1).Infof("Key pressed: %v", ev.Key())
-				clawStateReq := parseKBState(ev.Key())
+				glog.V(1).Infof("Key pressed: %v", ev)
+				clawStateReq := parseKBState(ev)
 				if err := stream.Send(clawStateReq); err != nil {
 					glog.Fatal(err)
 				}
